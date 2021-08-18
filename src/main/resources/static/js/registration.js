9829d039e6dcb609775453cd231ce27eb7d22823
Sanjaya Ghimire <sanjaya428@gmail.com>
2021-08-18 12:26:10 -0500

    some changes in registration.js file

--- src/main/resources/static/js/registration.js
+++ src/main/resources/static/js/registration.js
@@ -17,22 +17,22 @@ const content = `
   <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 </div>
 
-<div class="mb-3">
+<!--<div class="mb-3">
 <label for="username" class="form-label">Username </label><br> 
 <input name="username" class="form-control" id="username" pattern="[A-Za-z0-9]+">
 <div class="form-text"><i>(letters and numbers only, no punctuation or special characters)</i></div>
 
 
-</div>
+</div>-->
 
 <div class="mb-3">
   <label for="exampleInputPassword1" class="form-label">Password</label>
   <input type="password" class="form-control" id="exampleInputPassword1">
 </div>
-<div class="mb-3 form-check">
-  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-  <label class="form-check-label" for="exampleCheck1">Check me out</label>
-</div>
+<!--<div class="mb-3 form-check">-->
+<!--  <input type="checkbox" class="form-check-input" id="exampleCheck1">-->
+<!--  <label class="form-check-label" for="exampleCheck1">Check me out</label>-->
+<!--</div>-->
 <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
